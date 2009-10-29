0d4c28565ebd94de1c9b65aeaa9e3f6563ad7b84
Piotr Gabryjeluk <piotr@wikidot.com>
2009-10-29 14:31:12 +0100

    Added displaying of time data

--- pytyme.py
+++ pytyme.py
@@ -34,6 +34,25 @@ def set_current_task(task):
 def del_current_task():
     os.remove(CURRENT_TASK_PATH)
 
+def time_pretty(total):
+    seconds = int(total)
+    minutes = seconds / 60
+    seconds = seconds % 60
+    hours = minutes / 60
+    minutes = minutes % 60
+    
+    parts = []
+    if hours > 0:
+        parts.append(str(hours) + "h")
+    if minutes > 0:
+        parts.append(str(minutes) + "m")
+    
+    s = " ".join(parts)
+    if s == "":
+        return '0m'
+    else:
+        return s
+
 class Project(object):
     def __init__(self, name):
         self.name = name
@@ -60,6 +79,11 @@ class Project(object):
     def task(self, name):
         return self.current_period().task(name)
 
+    @classmethod
+    def all(cls):
+        for dir in [file for file in os.listdir(PROJECTS_DIR) if file[0] != '.' and os.path.isdir(os.path.join(PROJECTS_DIR, file))]:
+            yield Project(dir)
+
 
 class Period(object):
     def __init__(self, project, name):
@@ -75,6 +99,29 @@ class Period(object):
 
     def task(self, name):
         return Task(self, name)
+
+    def tasks(self):
+        for dir in [file for file in os.listdir(self.dir) if file[0] != '.' and os.path.isfile(os.path.join(self.dir, file))]:
+            yield Task(self, dir)
+
+    def is_active(self):
+        for task in self.tasks():
+            return True
+        return False
+
+    def total_time(self):
+        total = 0
+        for task in self.tasks():
+            total += task.total_time()
+        return total
+
+    def pretty_print(self):
+        return "Project: %s\nPeriod: %s\nTotal time: %s\n\n  %s" % (
+            self.project.name,
+            self.name.split(',')[1],
+            time_pretty(self.total_time()),
+            "\n\n  ".join([task.pretty_print().replace("\n", "\n  ") for task in self.tasks()])
+        )
 
 
 class Task(object):
@@ -98,16 +145,63 @@ class Task(object):
     def stop(self, when=NOW, comment=''):
         del_current_task()
         self.file.write(str(when) + ',' + comment + '\n')
+
+    def times(self):
+        try:
+            return self._times
+        except:
+            self._times = []
+            filero = file(self.filename, 'r')
+            for t in [line.split(',') for line in filero.read().split('\n')]:
+                try:
+                    r = {}
+                    r['time1f'] = float(t[0]) / 1000
+                    r['time1s'] = time.strftime('%Y-%m-%d %H:%M', time.gmtime(r['time1f']))
+                    try:
+                        r['time2f'] = float(t[1]) / 1000
+                        r['time2s'] = time.strftime('%Y-%m-%d %H:%M', time.gmtime(r['time2f']))
+                    except:
+                        r['time2f'] = NOW / 1000
+                        r['time2s'] = 'NOW'
+                    try:
+                        r['comment'] = t[2]
+                    except:
+                        r['comment'] = ''
+                    r['total'] = r['time2f'] - r['time1f']
+                    self._times.append(r)
+                except:
+                    pass
+            return self._times
+
+    def total_time(self):
+        total = 0
+        for time in self.times():
+            total += time['total']
+        return total
+
+    def pretty_print(self):
+        filero = file(self.filename, 'r')
+        times = ["%s - %s  %s" % (time['time1s'], time['time2s'], time['comment']) for time in self.times()]
+        return "Task: %s (total: %s)\n  %s" % (self.name, time_pretty(self.total_time()), "\n  ".join(times))
+
     
 class NoCurrentTask(Exception):
     pass
 
 make_pytyme_dirs()
 
-if sys.argv[1] == 'start':
+if len(sys.argv) == 1:
+    periods = []
+    for project in Project.all():
+        period = project.current_period()
+        if period.is_active():
+            periods.append(period)
+    print "\n" + "\n\n========================\n\n".join([period.pretty_print() for period in periods]) + "\n"
+    
+elif sys.argv[1] == 'start':
     project_name, task_name = sys.argv[2].split('/', 1)
     Project(project_name).task(task_name).start()
 
-if sys.argv[1] == 'stop':
+elif sys.argv[1] == 'stop':
     current_task().stop()
 
